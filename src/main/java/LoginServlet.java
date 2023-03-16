151080378159ff948ec8f71dfaac0e34996471e1
Jupiter_KD <DavidKwan3106@outlook.com>
2023-03-16 13:25:44 +0800

    add postgreSQL support

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -4,107 +4,101 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.*;
+import java.sql.*;
 import java.util.*;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class LoginServlet extends HttpServlet {
+
     private int IsBlockUser(String username) {
         try {
-            String blacklistPath = "D:\\00_code\\08_WebApp_LoginSys\\loginsys\\src\\main\\user_data\\blacklist.txt";
-            File blacklistFile = new File(blacklistPath);
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(blacklistFile));
-            String lineData;
-            while ((lineData = bufferedReader.readLine()) != null) {
-                if (lineData.contains(username)) {
-                    return 1;
-                }
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/simple_login_system", "postgres", "20029530");
+            ;
+            String sqlQuery = "SELECT * FROM block_users WHERE username=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                return 1;
+            } else {
+                return 0;
             }
-            return 0;
-        } catch (FileNotFoundException e) {
-            System.out.println("没有找到指定文件");
-            return -1;
-        } catch (IOException e) {
-            System.out.println("文件读写出错");
-            return -1;
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=utf-8");
+
         String username = request.getParameter("username");
         String pwd = request.getParameter("pwd");
+
         int isBlockUser = IsBlockUser(username);
-        if (isBlockUser == -1) {
-            System.out.println("系统内部错误！");
-            response.getWriter().print("<h2>三秒后自动返回登陆页面</h2>");
-            response.setHeader("refresh", "3;url=login.html");
-        } else if (isBlockUser == 1) {
+        if (isBlockUser == 1) {
             response.getWriter().print("<h2>" + username + "已被阻止登录！</h2>");
             response.getWriter().print("<h2>三秒后自动返回登陆页面</h2>");
             response.setHeader("refresh", "3;url=login.html");
+
         } else {
             try {
-                String userPath = "D:\\00_code\\08_WebApp_LoginSys\\loginsys\\src\\main\\user_data\\data.txt";
-                String bufferPath = "D:\\00_code\\08_WebApp_LoginSys\\loginsys\\src\\main\\user_data\\data_buffer.txt";
-                File txtFile = new File(userPath);
-                File bufferFile = new File(bufferPath);
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(txtFile));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter((bufferFile), false));
-                String lineData;
-                boolean isUserExist = false;
+                Class.forName("org.postgresql.Driver");
+                Connection connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/simple_login_system", "postgres", "20029530");
+                ;
+                String sqlQuery = "SELECT * FROM users WHERE username=? AND pwd=?";
+                PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, pwd);
+                ResultSet resultSet = preparedStatement.executeQuery();
 
-                while ((lineData = bufferedReader.readLine()) != null) {
-                    String[] detail = lineData.split(",");
-                    if (detail[0].equals(username)) {
-                        if (detail[1].equals(pwd)) {
-                            Date date = new Date(System.currentTimeMillis());
-                            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
-                            isUserExist = true;
-                            response.getWriter().print("<h2>" + username + "，你好！</h2>");
-                            String gender = (detail[2].equals("man") ? "男" : "女");
-                            detail[6] = String.valueOf(Integer.parseInt(detail[6]) + 1);
-                            response.getWriter().print("<h2>个人基本信息：</h2><ul><li>姓名：" + detail[0] + "</li><li>性别：" + gender + "</li><li>年龄：" + detail[3] + "</li><li>邮箱：" + detail[4] + "</li><li>注册时间：" + detail[5] + "</li><li>登录次数：" + detail[6] + "</li><li>上次登录时间：" + detail[7] + "</li><li>本次登录时间：" + date + "</li></ul>");
-
-                            response.getWriter().print("<h2>其它信息：</h2><ul><li>请求方式：" + request.getMethod() + "</li><li>请求端ip地址：" + request.getRemoteAddr() + "</li>");
-
-                            Enumeration<String> headers = request.getHeaderNames();
-                            while (headers.hasMoreElements()) {
-                                String value = request.getHeader(headers.nextElement());
-                                System.out.println(headers.nextElement() + ":" + value);
-                            }
-
-                            detail[7] = String.valueOf(date);
-                            String updateData = detail[0] + "," + detail[1] + "," + detail[2] + "," + detail[3] + "," + detail[4] + "," + detail[5] + "," + detail[6] + "," + detail[7];
-                            bufferedWriter.write(updateData);
-                            bufferedWriter.newLine();
-                        } else {
-                            response.getWriter().print("<h2>账号或密码错误！</h2>");
-                            response.getWriter().print("<h2>三秒后自动返回登陆页面</h2>");
-                            response.setHeader("refresh", "3;url=login.html");
-                        }
-                    } else {
-                        bufferedWriter.write(lineData);
-                        bufferedWriter.newLine();
+                if (resultSet.next()) {
+                    String[] detail = new String[resultSet.getMetaData().getColumnCount()];
+                    for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++){
+                        detail[i - 1] = resultSet.getString(i);
                     }
-                }
-                bufferedReader.close();
-                bufferedWriter.close();
-                txtFile.delete();
-                bufferFile.renameTo(new File(userPath));
+                    Date date = new Date(System.currentTimeMillis());
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+                    response.getWriter().print("<h2>" + username + "，你好！</h2>");
+                    String gender = (detail[2].equals("1") ? "男" : "女");
+                    detail[6] = String.valueOf(Integer.parseInt(detail[6]) + 1);
+                    response.getWriter().print("<h2>个人基本信息：</h2><ul><li>姓名：" + detail[0] + "</li><li>性别：" + gender + "</li><li>年龄：" + detail[3] + "</li><li>邮箱：" + detail[4] + "</li><li>注册时间：" + detail[5] + "</li><li>登录次数：" + detail[6] + "</li><li>上次登录时间：" + detail[7] + "</li><li>本次登录时间：" + date + "</li></ul>");
+                    response.getWriter().print("<h2>其它信息：</h2><ul><li>请求方式：" + request.getMethod() + "</li><li>请求端ip地址：" + request.getRemoteAddr() + "</li>");
 
-                if (!isUserExist) {
-                    response.getWriter().print("<h2>系统未找到" + username + "的信息！</h2>");
+                    Enumeration<String> headers = request.getHeaderNames();
+                    while (headers.hasMoreElements()) {
+                        String value = request.getHeader(headers.nextElement());
+                        System.out.println(headers.nextElement() + ":" + value);
+                    }
+
+                    detail[7] = String.valueOf(date);
+
+                    String sqlUpdateUser = "UPDATE users SET accessfreq = ?, lasttime = ? WHERE username = ?";
+                    preparedStatement = connection.prepareStatement(sqlUpdateUser);
+
+                    preparedStatement.setInt(1, Integer.parseInt(detail[6]));
+                    preparedStatement.setString(2, detail[7]);
+                    preparedStatement.setString(3, username);
+                    int result = preparedStatement.executeUpdate();
+
+                }else {
+                    response.getWriter().print("<h2>账号不存在或密码错误！</h2>");
                     response.getWriter().print("<h2>三秒后自动返回登陆页面</h2>");
                     response.setHeader("refresh", "3;url=login.html");
                 }
-            } catch (FileNotFoundException e) {
-                System.out.println("没有找到指定文件");
-            } catch (IOException e) {
-                System.out.println("文件读写出错");
+
+            } catch (SQLException | ClassNotFoundException | IOException | NumberFormatException e) {
+                throw new RuntimeException(e);
             }
         }
+
     }
 }
--- src/main/java/RegisterServlet.java
+++ src/main/java/RegisterServlet.java
@@ -3,10 +3,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.*;
 
+import java.io.*;
+import java.util.Date;
+import java.lang.*;
 
 public class RegisterServlet extends HttpServlet {
     @Override
@@ -15,42 +16,51 @@ public class RegisterServlet extends HttpServlet {
         response.setContentType("text/html;charset=utf-8");
 
         Date timestamp = new Date(System.currentTimeMillis());
-
         String username = request.getParameter("username");
         String pwd = request.getParameter("pwd");
         String gender = request.getParameter("gender");
-        String age = request.getParameter("age");
+        int age = Integer.parseInt(request.getParameter("age"));
         String email = request.getParameter("email");
 
-        try {
-            String txtPath = "D:\\00_code\\08_WebApp_LoginSys\\loginsys\\src\\main\\user_data\\data.txt";
-            File txtFile = new File(txtPath);
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(txtFile));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(txtFile, true));
-            String lineData;
-            boolean isUserExist = false;
-            while ((lineData = bufferedReader.readLine()) != null) {
-                String[] detail = lineData.split(",");
-                if (detail[0].equals(username)) {
-                    response.getWriter().print("<h2>" + username + "已经注册过了！</h2>");
-                    isUserExist = true;
-                    break;
-                }
-            }
+        Connection connection;
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
 
-            if (!isUserExist) {
-                bufferedWriter.newLine();
-                bufferedWriter.write(username + "," + pwd + "," + gender + "," + age + "," + email + "," + timestamp + ",0," + timestamp);
-                bufferedReader.close();
-                bufferedWriter.close();
-                response.getWriter().print("<h2>" + username + "注册成功！</h2>");
-                response.getWriter().print("<h2>三秒后自动返回登陆页面</h2>");
-                response.setHeader("refresh", "3;url=login.html");
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/simple_login_system", "postgres", "20029530");
+            String sqlQuery = "SELECT * FROM users WHERE username=?";
+            preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                response.getWriter().print("<h2>" + username + "已经注册过了！</h2>");
+            } else {
+                String sqlAddUser = "INSERT INTO users(username,pwd,gender,age,email,registime,lasttime) VALUES(?,?,?,?,?,?,?)";
+                preparedStatement = connection.prepareStatement(sqlAddUser);
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, pwd);
+                int sex = gender.equals("man") ? 1 : 0;
+                String date = String.valueOf(timestamp);
+                preparedStatement.setInt(3, sex);
+                preparedStatement.setInt(4, age);
+                preparedStatement.setString(5, email);
+                preparedStatement.setString(6, date);
+                preparedStatement.setString(7, date);
+                int result = preparedStatement.executeUpdate();
+                if (result == 1) {
+                    response.getWriter().print("<h2>" + username + "注册成功！</h2>");
+                    response.getWriter().print("<h2>三秒后自动返回登陆页面</h2>");
+                    response.setHeader("refresh", "3;url=login.html");
+                }
+
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("没有找到指定文件");
-        } catch (IOException e) {
-            System.out.println("文件读写出错");
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
